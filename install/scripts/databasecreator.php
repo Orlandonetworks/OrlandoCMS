c50c5e6a38923260674565f89ec159e74fa5b593
Stanisgrox <orlandonetworks@gmail.com>
2017-04-08 21:17:20 +0300

    0.0.1b
    Первичная поверка алгоритма создания таблиц

--- install/scripts/databasecreator.php
+++ install/scripts/databasecreator.php
@@ -1,6 +1,23 @@
 <?php
+session_start();
 
+$db_name = $_SESSION['dbname'];
+$db_login = $_SESSION['dblogin'];
+$db_password = $_SESSION['dbpassword'];
+$prefix = $_SESSION['dbprefix'];
 
+//Первичное присоединение
 
+$connection = mysql_connect("localhost", $db_login, $db_password);
+$db = mysql_select_db($db_name);
+mysql_set_charset("utf8");
 
+if(!$connection || !$db)
+{
+    exit(mysql_error());
+}
+
+mysql_query("CREATE TABLE "$prefix"test
+	test1 = varchar (15),
+	test2 = year")
 ?>
